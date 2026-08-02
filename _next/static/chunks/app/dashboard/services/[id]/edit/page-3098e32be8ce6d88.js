eb837af26bf9e7d90acfcbad150158d023b589ff
jkotosmos <jkotosmos@users.noreply.github.com>
2026-08-02 10:57:15 +0000

    deploy: da15ea4d138937228b35396966d25adf04cb3d4e

---
+++ _next/static/chunks/app/dashboard/services/[id]/edit/page-3098e32be8ce6d88.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[509],{},function(n){n.O(0,[590,438,550,971,117,744],function(){return n(n.s=3550)}),_N_E=n.O()}]);
---
+++ _next/static/dX1lr6FSN4g9OEQMaVu2D/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fdashboard\u002Fmessages\u002F[id]","\u002Fdashboard\u002Forders\u002F[id]","\u002Fdashboard\u002Fservices\u002F[id]\u002Fedit","\u002Ffreelancers\u002F[id]","\u002Fservices\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
